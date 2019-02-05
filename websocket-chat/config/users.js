39320260c40be2f3be4bb56733c20cc72054fe60
nostprumprajee <nostprumprajee@gmail.com>
2019-02-05 18:11:35 +0700

    edit

--- websocket-chat/config/users.js
+++ websocket-chat/config/users.js
@@ -1,13 +1,13 @@
 module.exports = [
   {
-    name: 'Rick',
-    lastName: 'Grimes',
+    name: 'Nost',
+    lastName: 'Nonsty',
     statusText: 'I am the leader!',
     image: 'users/rick.jpg'
   },
   {
-    name: 'Daryl',
-    lastName: 'Dixon',
+    name: 'Feii',
+    lastName: 'Nonsty',
     statusText: 'I like smashing Walkers.',
     image: 'users/daryl.jpg'
   },
